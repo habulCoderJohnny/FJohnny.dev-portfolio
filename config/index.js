95768a21e9598058a657852ceaf9b607bfd42b52
habulCoderJohnny <fokruljohnny@gmail.com>
2023-08-21 17:34:44 +0600

    Email template setup done

---
+++ config/index.js
@@ -0,0 +1,7 @@
+// EMAILJS Config Data
+
+export const EMAILJS_TEMPLATE_ID = "template_hf000s4"
+
+export const EMAILJS_SERVICE_ID = "service_t045qni"
+
+export const EMAILJS_PUBLIC_KEY = "aKGKxw2JQKU9cLf9_"
